1a34d235baa1cb36c23202e15332befcfd1fed8e
Mosihuddin <sheikhkinu@gmail.com>
2023-01-31 23:54:00 +0600

    revise

--- phitron2.c
+++ phitron2.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+int main (){
 
+   int arr[]={1,2,3,4,5};
+   int length = sizeof(arr)/sizeof (arr[0]);
+   printf("\n");
+   for(int i=0;i<length;i++){
+
+    printf("%d",arr[i]);
+   }
+   printf("\n");
+   for(int i= length-1;i>=0; i--){
+
+    printf("%d",arr[i]);
+   }
+
+   return 0;
+}
